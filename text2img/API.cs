2e86a130ff7b59cb0887fb6b64a0203a9d33f140
Kevin Gut <kevin.gut@bitm.sg>
2017-01-08 18:50:09 +0100

    Intellisense

--- text2img/API.cs
+++ text2img/API.cs
@@ -3,16 +3,28 @@ using System.Runtime.InteropServices;
 
 namespace text2img
 {
+    /// <summary>
+    /// Provides additional console features
+    /// </summary>
     public static class ConsoleEx
     {
+        /// <summary>
+        /// Returns true, if the output stream is redirected
+        /// </summary>
         public static bool IsOutputRedirected
         {
             get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdout)); }
         }
+        /// <summary>
+        /// Returns true, if the input stream is redirected
+        /// </summary>
         public static bool IsInputRedirected
         {
             get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdin)); }
         }
+        /// <summary>
+        /// Returns true, if the error stream is redirected
+        /// </summary>
         public static bool IsErrorRedirected
         {
             get { return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stderr)); }
--- text2img/Tools.cs
+++ text2img/Tools.cs
@@ -6,15 +6,36 @@ using System.IO;
 
 namespace text2img
 {
+    /// <summary>
+    /// Provides stream Functionality
+    /// </summary>
     public static class Tools
     {
+        /// <summary>
+        /// Default stream buffer
+        /// </summary>
         private const int DEF_BUFFER = 1024 * 1024;
 
+        /// <summary>
+        /// Copies a stream onto another
+        /// </summary>
+        /// <param name="Source">Source stream</param>
+        /// <param name="Destination">Destination stream</param>
+        /// <remarks>This will not seek back</remarks>
+        /// <returns>Number of bytes copied</returns>
         public static long CopyTo(this Stream Source, Stream Destination)
         {
             return CopyTo(Source, Destination, DEF_BUFFER);
         }
 
+        /// <summary>
+        /// Copies a stream onto another
+        /// </summary>
+        /// <param name="Source">Source stream</param>
+        /// <param name="Destination">Destination stream</param>
+        /// <param name="BufferSize">Size of the copy buffer</param>
+        /// <remarks>This will not seek back</remarks>
+        /// <returns>Number of bytes copied</returns>
         public static long CopyTo(this Stream Source, Stream Destination, int BufferSize)
         {
             long total=0;
@@ -28,13 +49,30 @@ namespace text2img
             return total;
         }
 
+        /// <summary>
+        /// MemoryStream.ToArray() for all streams
+        /// </summary>
+        /// <param name="Source">Stream to read</param>
+        /// <remarks>Will not seek back after reading</remarks>
+        /// <returns>Byte array with stream contents</returns>
         public static byte[] GetBytes(this Stream Source)
         {
             return GetBytes(Source, DEF_BUFFER);
         }
 
+        /// <summary>
+        /// MemoryStream.ToArray() for all streams
+        /// </summary>
+        /// <param name="Source">Stream to read</param>
+        /// <param name="BufferSize">The read buffer size</param>
+        /// <remarks>Will not seek back after reading</remarks>
+        /// <returns>Byte array with stream contents</returns>
         public static byte[] GetBytes(this Stream Source, int BufferSize)
         {
+            if (Source is MemoryStream)
+            {
+                return ((MemoryStream)Source).ToArray();
+            }
             using (MemoryStream MS = new MemoryStream())
             {
                 Source.CopyTo(MS, BufferSize);
