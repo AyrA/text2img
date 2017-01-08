281f66098032fb880a72c9fe176e39bfd4771c19
Kevin Gut <kevin.gut@bitm.sg>
2017-01-08 18:50:35 +0100

    Intellisense here too

--- text2img/TextParser.cs
+++ text2img/TextParser.cs
@@ -6,28 +6,50 @@ using System.IO;
 
 namespace text2img
 {
+    /// <summary>
+    /// Text parser for different encodings
+    /// </summary>
     public static class TextParser
     {
         private const string CR = "\r";
         private const string LF = "\n";
         private const string CRLF = "\r\n";
 
+        /// <summary>
+        /// Checks if an UTF-8 BOM is present
+        /// </summary>
+        /// <param name="SourceContent">Source stream</param>
+        /// <remarks>This will rewind the stream to the position it was before reading if possible</remarks>
+        /// <returns>True, if BOM is present</returns>
         public static bool IsUtf8(Stream SourceContent)
         {
             long Pos = SourceContent.Position;
             byte[] Data = new byte[3];
             if (SourceContent.Read(Data, 0, 3) == 3)
             {
-                SourceContent.Seek(-3, SeekOrigin.Current);
+                if (SourceContent.CanSeek)
+                {
+                    SourceContent.Seek(-3, SeekOrigin.Current);
+                }
                 return
                     Data[0] == 0xEF &&
                     Data[1] == 0xBB &&
                     Data[2] == 0xBF;
             }
-            SourceContent.Position = Pos;
+            if (SourceContent.CanSeek)
+            {
+                SourceContent.Position = Pos;
+            }
             return false;
         }
 
+        /// <summary>
+        /// Reads and converts text from a stream
+        /// </summary>
+        /// <param name="SourceContent">Text content</param>
+        /// <param name="SourceEncoding">Encoding the text is in</param>
+        /// <param name="ForceEncoding">Force the encoding (skips UTF-8 check if true)</param>
+        /// <returns>C# usable string</returns>
         public static string GetText(Stream SourceContent, Encoding SourceEncoding, bool ForceEncoding)
         {
             Encoding E = null;
@@ -43,6 +65,13 @@ namespace text2img
             return E.GetString(SourceContent.GetBytes());
         }
 
+        /// <summary>
+        /// converts text
+        /// </summary>
+        /// <param name="Data">Text content</param>
+        /// <param name="SourceEncoding">Encoding the text is in</param>
+        /// <param name="ForceEncoding">Force the encoding (skips UTF-8 check if true)</param>
+        /// <returns>C# usable string</returns>
         public static string GetText(byte[] Data, Encoding SourceEncoding, bool ForceEncoding)
         {
             using (MemoryStream MS = new MemoryStream(Data, false))
@@ -51,6 +80,12 @@ namespace text2img
             }
         }
 
+        /// <summary>
+        /// Fixes CRLF issues. (Converts Mac and Linux linebreaks to Windows line breaks)
+        /// </summary>
+        /// <remarks>This can work with files that contain mixed line breaks</remarks>
+        /// <param name="Source">Text to normalize</param>
+        /// <returns>CRLF-Fixed text</returns>
         public static string FixCrLf(string Source)
         {
             return Source.Replace(CRLF, CR).Replace(CR[0], LF[0]).Replace(LF, CRLF);
