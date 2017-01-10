f2d6fbdeb2a47f3558d5c1b092bc105c38f5552a
Kevin Gut <kevin.gut@bitm.sg>
2017-01-10 08:21:50 +0100

    Extension methods for Data URL and color duplication

--- text2img/Tools.cs
+++ text2img/Tools.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace text2img
 {
@@ -78,6 +80,68 @@ namespace text2img
                 Source.CopyTo(MS, BufferSize);
                 return MS.ToArray();
             }
+        }
+
+        public static Color Dupe(this Color C)
+        {
+            return Color.FromArgb(C.ToArgb());
+        }
+
+        public static string ToDataUrl(this Image I, ImageFormat Format)
+        {
+            string URL = string.Format("data:image/{0};base64,", GetMediaType(Format));
+            using (MemoryStream MS = new MemoryStream())
+            {
+                I.Save(MS, Format);
+                return URL + Convert.ToBase64String(MS.ToArray());
+            }
+        }
+
+        public static string ToDataUrl(this Bitmap B, ImageFormat Format)
+        {
+            return ((Image)B).ToDataUrl(Format);
+        }
+
+        private static string GetMediaType(ImageFormat Format)
+        {
+            //These are all untested
+            if (Format.Guid == ImageFormat.Bmp.Guid)
+            {
+                return "bmp";
+            }
+            if (Format.Guid == ImageFormat.Emf.Guid)
+            {
+                return "emf";
+            }
+            if (Format.Guid == ImageFormat.Exif.Guid)
+            {
+                return "exif";
+            }
+            if (Format.Guid == ImageFormat.Gif.Guid)
+            {
+                return "gif";
+            }
+            if (Format.Guid == ImageFormat.Icon.Guid)
+            {
+                return "ico";
+            }
+            if (Format.Guid == ImageFormat.Jpeg.Guid)
+            {
+                return "jpg";
+            }
+            if (Format.Guid == ImageFormat.Png.Guid)
+            {
+                return "png";
+            }
+            if (Format.Guid == ImageFormat.Tiff.Guid)
+            {
+                return "tiff";
+            }
+            if (Format.Guid == ImageFormat.Wmf.Guid)
+            {
+                return "wmf";
+            }
+            throw new ArgumentException("Unsupported image type for data URL");
         }
     }
 }
